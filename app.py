0d5ef72a77c6a2008ee8c6544775307da2894318
rmasonh18 <rh12@emil.sc.edu>
2019-12-30 19:16:25 -0500

    make a sidecar

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 # root or homepage
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 # POST METHOD
 @app.route("/hello", methods=["POST"])
